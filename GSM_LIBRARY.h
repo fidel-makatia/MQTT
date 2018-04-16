3c2cad99ca5a49ab44fa1706b93a302fc0280da5
Oki Agaya Okwiri <okiokwiri@gmail.com>
2018-04-16 12:47:06 +0000

    GSM_LIBRARY.h added commands for multiple connections

--- GSM_LIBRARY.h
+++ GSM_LIBRARY.h
@@ -41,6 +41,13 @@ void Put_AT_CIPMUX(void)
 		uart_0_print_char(13);
 		uart_0_write("\n");
 }
+void PUT_AT_CIPMUX_MULTI(void)
+{
+	uart_0_clear_buffer();
+	uart_0_write("AT+CIPMUX=1");
+	uart_0_print_char(13);
+	uart_0_write("\n");
+}
 void Put_AT_CGATT(void)
 {
 		uart_0_clear_buffer();
@@ -111,6 +118,212 @@ void Put_AT_CIPSTART(char *IP_ADDRESS, char *PORT_NUMBER)
 		uart_0_print_char(34);
 		uart_0_print_char(13); //cr
 		uart_0_write("\n");
+}
+void PUT_AT_CIPSTART_MULTIPLE(int connection, char *IP_ADDRESS, char *PORT_NUMBER)
+{
+	switch(connection)
+	{
+		case(0):
+		uart_0_clear_buffer();
+		uart_0_write("AT+CIPSTART=0");
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write("TCP");
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(IP_ADDRESS);
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(PORT_NUMBER);
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(13); //cr
+		uart_0_write("\n");
+		break;
+		case(1):
+		uart_0_clear_buffer();
+		uart_0_write("AT+CIPSTART=1");
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write("TCP");
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(IP_ADDRESS);
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(PORT_NUMBER);
+		uart_0_print_char(34);//quotation mark
+		uart_0_print_char(13); //cr
+		uart_0_write("\n");
+		break;
+		case(2):
+		uart_0_clear_buffer();
+		uart_0_write("AT+CIPSTART=2");
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write("TCP");
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(IP_ADDRESS);
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44);  //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(PORT_NUMBER);
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(13);
+		uart_0_write("\n");
+		break;
+		case(3):
+		uart_0_clear_buffer();
+		uart_0_write("AT+CIPSTART=3");
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_write("TCP");
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(IP_ADDRESS):
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(44); //comma
+		uart_0_print_char(34); //quotation mark
+		uart_0_write(PORT_NUMBER);
+		uart_0_print_char(34); //quotation mark
+		uart_0_print_char(13);
+		uart_0_write("\n"):
+		break;
+		case(4):
+		uart_0_clear_buffer();
+		uart_0_write("AT+CIPSTART=4");
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_write("TCP");
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(IP_ADDRESS):
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(PORT_NUMBER);
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(13);
+                uart_0_write("\n"):
+                break;
+		case(5):
+                uart_0_clear_buffer();
+                uart_0_write("AT+CIPSTART=5");
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_write("TCP");
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(IP_ADDRESS):
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(PORT_NUMBER);
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(13);
+                uart_0_write("\n"):
+                break;
+		case(6):
+                uart_0_clear_buffer();
+                uart_0_write("AT+CIPSTART=6");
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_write("TCP");
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(IP_ADDRESS):
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(PORT_NUMBER);
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(13);
+                uart_0_write("\n"):
+                break;
+                case(7):
+                uart_0_clear_buffer();
+                uart_0_write("AT+CIPSTART=7");
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_write("TCP");
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(IP_ADDRESS):
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(44); //comma
+                uart_0_print_char(34); //quotation mark
+                uart_0_write(PORT_NUMBER);
+                uart_0_print_char(34); //quotation mark
+                uart_0_print_char(13);
+                uart_0_write("\n"):
+                break;
+
+
+	}
+}
+void PUT_AT_CIPEND_MULTI(int connection)
+{
+	switch(connection)
+		{
+		case(0):
+                   uart_0_clear_buffer();
+		   uart_0_write("AT+CIPSEND=0");
+		   uart_0_print_char(13);
+		   uart_0_write("\n");
+		break;
+		case(1):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=1");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		case(2):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=2");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		case(3):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=3");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		case(4):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=4");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		case(5):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=5");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		case(6):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=6");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		case(7):
+		  uart_0_clear_buffer();
+		  uart_0_write("AT+CIPSEND=7");
+		  uart_0_print_char(13);
+		  uart_0_write("\n"):
+		break;
+		}
 }
 void Put_AT_CIPSEND(void)
 {
